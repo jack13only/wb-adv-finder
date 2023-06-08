5bf798071c0b049f01e38bde4382c5daf40d34a9
Jack <jack13only@gmail.com>
2023-06-08 22:35:28 +0300

    feat: fix bug with db

--- app.js
+++ app.js
@@ -42,33 +42,42 @@
       const db = event.target.result;
       const transaction = db.transaction(['adverts_info'], 'readonly');
       const objectStore = transaction.objectStore('adverts_info');
-      const getRequest = objectStore.get(object_store_name);
+      const getRequestAll = objectStore.getAll();
+      getRequestAll.onsuccess = function (event) {
+        if (event.target.result && event.target.result.find((item) => item.id === object_store_name) != -1) {
+          const getRequest = objectStore.get(object_store_name);
 
-      getRequest.onsuccess = function (event) {
-        const data = event.target.result.model.advData.adverts;
-        elementList.forEach((card) => {
-          const item = data.find((item) => item.id === +card.getAttribute('data-nm-id'))
-          const cpm = item ? item.cpm : 'unknown'
-          const div = document.createElement("div");
-          div.innerText = `${cpm}`
-          div.style.position = 'absolute'
-          div.style.bottom = 0
-          div.style.right = 0
-          div.style.padding = '2px 4px'
-          div.style.backgroundColor = 'rgb(102, 255, 102)'
-          div.style.borderTopLeftRadius = '10px'
-
-          card.style.border = '3px solid rgb(102, 255, 102)'
-          card.style.borderRadius = '10px'
-          card.style.position = 'relative'
-          card.appendChild(div)
-        })
-      };
-
-      getRequest.onerror = function (event) {
-        console.error('Error retrieving objects:', event.target.error);
-      };
-
+          getRequest.onsuccess = function (event) {
+            const data = event?.target?.result?.model?.advData.adverts;
+            if (data) {
+              elementList.forEach((card) => {
+                const item = data.find((item) => item.id === +card.getAttribute('data-nm-id'))
+                const cpm = item ? item.cpm : 'unknown'
+                const div = document.createElement("div");
+                div.innerText = `Реклама: ${cpm}₽`
+      
+                div.style.color = 'white'
+                div.style.position = 'absolute'
+                div.style.bottom = 0
+                div.style.right = 0
+                div.style.padding = '2px 4px'
+                div.style.backgroundColor = 'rgb(102, 255, 102)'
+                div.style.borderTopLeftRadius = '10px'
+      
+                card.style.border = '3px solid rgb(102, 255, 102)'
+                card.style.borderRadius = '10px'
+                card.style.position = 'relative'
+                card.appendChild(div)
+              })
+            }            
+          };
+    
+          getRequest.onerror = function (event) {
+            console.error('Error retrieving objects:', event.target.error);
+          };
+        }
+      }     
+      
       transaction.oncomplete = function () {
         db.close();
       };
@@ -78,5 +87,4 @@
       console.error('Error opening database:', event.target.error);
     };
   }
-
 }())
--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 let currentUrl = '';
 
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-  if (changeInfo.status === "complete" && tab.url.startsWith("https://www.wildberries") && currentUrl !== tab.url) {
+  if (changeInfo.status === "complete" && tab.url.startsWith("https://www.wildberries.ru/catalog/0/search.aspx?search=") && currentUrl !== tab.url) {
     currentUrl = tab.url
     chrome.scripting.executeScript({
       target: {tabId, allFrames: true},
