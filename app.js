c29156f325e18820f4b0429d405a2db437831708
Jack <jack13only@gmail.com>
2023-06-11 17:12:18 +0300

    feat: use mutation observer

--- app.js
+++ app.js
@@ -1,35 +1,41 @@
 (function main() {
-  
-  const callLimit = 15
-  let isRunning = false
   let elementList = []
-  let count = 0
+  let divIdList = []
+
+  const generalObserver = new MutationObserver((mut) => mut.forEach((item) =>  {
+    if ([...item.addedNodes].findIndex((i) => i.className === 'catalog-page__content') !== -1) {
+      getProductCardList()
+    }
+  }))
+  
+  generalObserver.observe(document.getElementById('app'), {
+    childList: true, 
+    subtree: true,
+    attributes: false,
+    characterDataOldValue: false 
+  });
 
   function getElements() {
-    setTimeout(function runTimeout() {
-      if (count < callLimit && elementList.length === 0) {
-        elementList = document.querySelectorAll('[class*="-adv"]');
-        setTimeout(runTimeout, 500);
-      } else {
-        isRunning = false
-        if (elementList.length !== 0) getDataFromDb()
-      }
-      count++;
-    }, 500);
+    elementList = document.querySelectorAll('[class*="--adv"]');
+    divIdList = divIdList.filter((divId) => [...elementList].find((el) => +el.getAttribute('data-nm-id') === divId))
+    if (elementList.length !== 0) getDataFromDb()
   }
 
-  getElements()
-
-  addEventListener("scroll", () => {
-    if (!isRunning) {
-      isRunning = true
-      setTimeout(() => {
-        elementList = document.querySelectorAll('[class*="-adv"]')
-        isRunning = false
-        if (elementList.length !== 0) getDataFromDb()
-      }, 500)
-    }
-  });
+  function getProductCardList() {
+    const productCardList = document.querySelectorAll('.product-card-list')
+    
+    getElements()
+    
+    productCardList.forEach((productCard) => {
+      const productCardObserver = new MutationObserver(getElements)
+      productCardObserver.observe(productCard, {
+        childList: true, 
+        subtree: true,
+        attributes: false,
+        characterDataOldValue: false 
+      });  
+    })
+  }
 
   function getDataFromDb() {
     let params = new URL(document.location).searchParams
@@ -43,6 +49,7 @@
       const transaction = db.transaction(['adverts_info'], 'readonly');
       const objectStore = transaction.objectStore('adverts_info');
       const getRequestAll = objectStore.getAll();
+
       getRequestAll.onsuccess = function (event) {
         if (event.target.result && event.target.result.find((item) => item.id === object_store_name) != -1) {
           const getRequest = objectStore.get(object_store_name);
@@ -51,23 +58,34 @@
             const data = event?.target?.result?.model?.advData.adverts;
             if (data) {
               elementList.forEach((card) => {
-                const item = data.find((item) => item.id === +card.getAttribute('data-nm-id'))
-                const cpm = item ? item.cpm : 'unknown'
-                const div = document.createElement("div");
-                div.innerText = `Реклама: ${cpm}₽`
-      
-                div.style.color = 'white'
-                div.style.position = 'absolute'
-                div.style.bottom = 0
-                div.style.right = 0
-                div.style.padding = '2px 4px'
-                div.style.backgroundColor = 'rgb(102, 255, 102)'
-                div.style.borderTopLeftRadius = '10px'
-      
-                card.style.border = '3px solid rgb(102, 255, 102)'
-                card.style.borderRadius = '10px'
-                card.style.position = 'relative'
-                card.appendChild(div)
+                const cardNmId = +card.getAttribute('data-nm-id')
+                const item = data.find((item) => item.id === cardNmId) 
+                if (!divIdList.includes(cardNmId)) {
+                  divIdList.push(cardNmId)
+                  const div = document.createElement("div");
+                  
+                  const cpm = item ? item.cpm : 'unknown'
+                  div.innerText = `Реклама: ${cpm}₽`
+        
+                  div.style.color = 'white'
+                  div.style.position = 'absolute'
+                  div.style.bottom = 0
+                  div.style.right = 0
+                  div.style.padding = '2px 4px'
+                  div.style.backgroundColor = 'rgba(87, 222, 87, 0.8)'
+                  div.style.borderTopLeftRadius = '10px'
+                  div.style.borderBottomRightRadius = '10px'
+                  div.style.display = 'flex'
+                  div.style.justifyContent = 'center'
+                  div.style.alignItems = 'center'
+                  div.style.fontSize = '12px'
+
+                  const cardWrapper = card.querySelector('.product-card__wrapper')
+                  
+                  cardWrapper.style.border = '2px solid rgba(87, 222, 87, 0.8)'
+                  cardWrapper.style.borderRadius = '10px'
+                  cardWrapper.appendChild(div)
+                }
               })
             }            
           };
